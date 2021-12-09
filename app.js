0a49a61fc3b2a3d0f8f9d2bcfe21f5075fde37e6
Alireza Jafari <asjn3e@gmail.com>
2021-12-09 21:06:53 +0330

    add repeat index function

--- app.js
+++ app.js
@@ -4,16 +4,32 @@ class DrumKit{
         this.difficultyBox=document.querySelectorAll(".difficultyBox__input");
         this.articles=document.querySelectorAll(".proRequierd");
         this.difficulty="lite";
+        this.Interval=null;
+        this.bpm=80;
+        this.index=1;
         this.difficultySwitcher = (value)=>{
             this.difficulty=value;
             this.sequencer.classList.toggle("Pro")
             
+        }
+        this.repeat=(x)=>{
+            x %= 10;
+            console.log(x);
+            document.querySelectorAll(`.b${x}`)
+        }
+        this.play=()=>{
+            const beatPerMinute=60 / this.bpm * 1000;
+            this.Interval=setInterval(() => {
+                this.repeat(this.index);
+                this.index++;
+            }, beatPerMinute);
         }
 
     }
 }
 const drumkit=new DrumKit();
 
+drumkit.play();
 //event listeners
 drumkit.difficultyBox.forEach((element)=>{
     element.addEventListener("input",(e)=>{
